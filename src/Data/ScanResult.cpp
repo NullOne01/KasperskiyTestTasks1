55e92883ee19138874095cf51d406405ef86a138
nullone <adviventes@gmail.com>
2022-10-13 05:49:39 +0300

    Now file checks are passed into final result

--- src/Data/ScanResult.cpp
+++ src/Data/ScanResult.cpp
@@ -3,23 +3,43 @@
 #include "ScanResult.h"
 #include "../Utils/ChronoUtils.h"
 
-ScanResult::ScanResult() {}
-
 ScanResult::operator std::string() const {
-    const auto[hrs, mins, secs, ms] = ChronoUtils::chronoBurst(execution_time);
+    const auto[hrs, mins, secs, ms] = ChronoUtils::chronoBurst(execution_time_);
 
     std::ostringstream out;
-    out << "Processed files: " << processed_files << "\n"
-        << "JS detects: " << js_detects << "\n"
-        << "Unix detects: " << unix_detects << "\n"
-        << "macOS detects: " << macos_detects << "\n"
-        << "Errors:" << errors_num << "\n"
-        << "Execution time: " << std::setw(2) << hrs.count() << ":"
+    out << "Processed files: " << processed_files_ << "\n"
+        << "JS detects: " << js_detects_ << "\n"
+        << "Unix detects: " << unix_detects_ << "\n"
+        << "macOS detects: " << macos_detects_ << "\n"
+        << "Errors: " << errors_num_ << "\n"
+        << "Execution time: " << std::setfill('0') << std::setw(2) << hrs.count() << ":"
                               << std::setw(2) << mins.count() << ":"
                               << std::setw(2) << secs.count() << ":"
-                              << std::setw(5) << ms.count() << "\n";
+                              << std::setw(4) << ms.count() << "\n";
     return out.str();
 }
+
 void ScanResult::setExecutionTime(const std::chrono::microseconds &execution_time) {
-    ScanResult::execution_time = execution_time;
+    execution_time_ = execution_time;
+}
+
+void ScanResult::loadParseResult(ParseFileResult parseFileResult) {
+    processed_files_++;
+
+    switch (parseFileResult) {
+        case ParseFileResult::ERROR:
+            errors_num_++;
+            break;
+        case MALWARE_JS:
+            js_detects_++;
+            break;
+        case MALWARE_UNIX:
+            unix_detects_++;
+            break;
+        case MALWARE_MACOS:
+            macos_detects_++;
+            break;
+        case CLEAR:
+            break;
+    }
 }
--- src/Data/ScanResult.h
+++ src/Data/ScanResult.h
@@ -3,22 +3,23 @@
 
 #include <chrono>
 #include <string>
+#include "ParseFileResult.h"
 
 class ScanResult {
 private:
-    int processed_files = 0;
-    int js_detects = 0;
-    int unix_detects = 0;
-    int macos_detects = 0;
-    int errors_num = 0;
-    std::chrono::microseconds execution_time{0};
+    int processed_files_ = 0;
+    int js_detects_ = 0;
+    int unix_detects_ = 0;
+    int macos_detects_ = 0;
+    int errors_num_ = 0;
+    std::chrono::microseconds execution_time_{0};
 
 public:
-    ScanResult();
-
     operator std::string() const;
 
     void setExecutionTime(const std::chrono::microseconds &execution_time);
+
+    void loadParseResult(ParseFileResult parseFileResult);
 };
 
 #endif //KASPERSKIYTEST1_SRC_SCANRESULT_H_
